8de6437f9f0a2eeced2ae0443244f7db88bab321
joellen <joellen.carter@versionone.com>
2011-05-19 14:26:47 -0400

    some cleaning up

--- write_plan_mindmap.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH <<File.expand_path('../lib',__FILE__)
-require 'mindmap'
-
-plan = 'RegressionPlan:210635'
-
-mm = MindMap.new('http://www7.v1host.com/V1Production/', 'joellen', 'carter')
-
-mm.write_plan_data(plan)
-
